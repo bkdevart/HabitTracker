a79dfff829ceafa7b7a1d20fdd352d1110e742b2
Brandon Knox <bknox.dev@gmail.com>
2021-04-11 16:38:28 -0700

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Brandon Knox on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
